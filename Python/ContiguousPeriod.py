9ec4e6eeaecc501b5b31cca562ca9b03ae5c55f1
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2019-05-27 15:22:44 +0200

    return same period when having one period

--- Python/ContiguousPeriod.py
+++ Python/ContiguousPeriod.py
@@ -1,12 +1,20 @@
 import unittest
 
+class Period :
+    def __init__(self):
+        return
+
 class TestContiguousPeriod(unittest.TestCase):
     
     def CalculateContiguousPeriods(self, periods):
-        return []
+        return periods;
 
     def test_should_return_no_period_when_having_empty_collection(self):
         assert self.CalculateContiguousPeriods([]) == []
+
+    def test_should_return_same_period_when_having_one_period(self):
+        period = Period()
+        assert self.CalculateContiguousPeriods([period]) == [period]
     
 if __name__ == '__main__':
     unittest.main()
