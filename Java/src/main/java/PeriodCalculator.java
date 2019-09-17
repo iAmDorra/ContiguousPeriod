8bff54b3596a899ec1538445aa1a882c668534fc
fabien.giannesini <fabien.giannesini@gmail.com>
2019-09-17 23:25:29 +0200

    Add fail fast on merge continuous periods

--- Java/src/main/java/PeriodCalculator.java
+++ Java/src/main/java/PeriodCalculator.java
@@ -6,16 +6,16 @@ import java.util.stream.Stream;
 class PeriodCalculator {
 
     List<Period> mergeContiguousPeriods(List<Period> periods) {
-        if(periods.size()> 1) {
-            List<Period> zeroPeriods = periods.stream()
-                    .filter(p1 -> p1.getRate() == 0)
-                    .sorted()
-                    .collect(Collectors.toList());
-            Stream<Period> mergedPeriods = mergeZeroRateContiguousPeriods(zeroPeriods);
-            Stream<Period> nonZeroRatePeriods = periods.stream().filter(p -> p.getRate() != 0);
-            return Stream.concat(nonZeroRatePeriods, mergedPeriods).collect(Collectors.toList());
+        if (periods.size() <= 1) {
+            return periods;
         }
-        return periods;
+        List<Period> zeroPeriods = periods.stream()
+                .filter(p1 -> p1.getRate() == 0)
+                .sorted()
+                .collect(Collectors.toList());
+        Stream<Period> mergedPeriods = mergeZeroRateContiguousPeriods(zeroPeriods);
+        Stream<Period> nonZeroRatePeriods = periods.stream().filter(p -> p.getRate() != 0);
+        return Stream.concat(nonZeroRatePeriods, mergedPeriods).collect(Collectors.toList());
     }
 
     private Stream<Period> mergeZeroRateContiguousPeriods(List<Period> zeroRatePeriods) {
