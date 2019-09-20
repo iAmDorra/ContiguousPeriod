7f15d2e44c96784c2d337f637245123357b6ed3f
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2019-09-20 15:53:12 +0200

    Rename some methods and variables and use set instead of remove and add

--- Java/src/main/java/PeriodCalculator.java
+++ Java/src/main/java/PeriodCalculator.java
@@ -10,7 +10,7 @@ public class PeriodCalculator {
             Stream<RatedPeriod> zeroPeriods = periods.stream()
                     .filter(p1 -> p1.IsRateEqualTo(0))
                     .sorted(getPeriodComparator());
-            Stream<RatedPeriod> mergedPeriods = mergeZeroRateContiguousPeriods(zeroPeriods);
+            Stream<RatedPeriod> mergedPeriods = mergePeriods(zeroPeriods);
             Stream<RatedPeriod> nonZeroRatePeriods = periods.stream().filter(p -> !p.IsRateEqualTo(0));
             return Stream.concat(nonZeroRatePeriods, mergedPeriods).collect(Collectors.toList());
         }
@@ -22,19 +22,18 @@ public class PeriodCalculator {
         return (RatedPeriod p1, RatedPeriod p2) -> p1.before(p2)? -1: 1;
     }
 
-    private Stream<RatedPeriod> mergeZeroRateContiguousPeriods(Stream<RatedPeriod> zeroPeriods) {
-        RatedPeriod[] zeroRatePeriods = zeroPeriods.toArray(RatedPeriod[]::new);
+    private Stream<RatedPeriod> mergePeriods(Stream<RatedPeriod> periods) {
+        RatedPeriod[] ratedPeriods = periods.toArray(RatedPeriod[]::new);
         List<RatedPeriod> mergedPeriods = new ArrayList<>();
         int mergedPeriodIndex = 0;
-        mergedPeriods.add(zeroRatePeriods[0]);
-        for (int periodIndex = 1; periodIndex < zeroRatePeriods.length; periodIndex++) {
-            if (mergedPeriods.get(mergedPeriodIndex).isContiguousTo(zeroRatePeriods[periodIndex])) {
-                RatedPeriod mergedPeriod =  mergedPeriods.get(mergedPeriodIndex).merge(zeroRatePeriods[periodIndex]);
-                mergedPeriods.remove(mergedPeriodIndex);
-                mergedPeriods.add(mergedPeriod);
+        mergedPeriods.add(ratedPeriods[0]);
+        for (int periodIndex = 1; periodIndex < ratedPeriods.length; periodIndex++) {
+            if (mergedPeriods.get(mergedPeriodIndex).isContiguousTo(ratedPeriods[periodIndex])) {
+                RatedPeriod mergedPeriod =  mergedPeriods.get(mergedPeriodIndex).merge(ratedPeriods[periodIndex]);
+                mergedPeriods.set(mergedPeriodIndex, mergedPeriod);
             }
             else {
-                mergedPeriods.add(zeroRatePeriods[periodIndex]);
+                mergedPeriods.add(ratedPeriods[periodIndex]);
                 mergedPeriodIndex++;
             }
         }
